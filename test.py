c59318c772e614ff321aa27e58b7146949d33642
ghost2442 <68842877+ghost2442@users.noreply.github.com>
2023-04-16 11:05:36 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,10 @@ from telethon.sync import TelegramClient
 #import logging
 #logging.basicConfig(level=logging.DEBUG)
 
+def main():
+  print('lets go')
+  client = TelegramClient('19', 12899094, '335e73259ab7fe48c3d12e4139f6dfa0') 
+  client.connect()
+  print('connected')
 
-client = TelegramClient('19', 12899094, '335e73259ab7fe48c3d12e4139f6dfa0')
-
-client.connect()
-print('connected')
+main()
