69b26807f636a46acf6f4c1301ab7214fbb5519d
ghost2442 <68842877+ghost2442@users.noreply.github.com>
2023-06-07 11:25:07 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 from telethon.sync import TelegramClient
+from flask import Flask
 #import logging
 #logging.basicConfig(level=logging.DEBUG)
-
-def main():
+app=Flask(__name__)
+@app.route('/')
+def man():
   print('lets go')
-  client = TelegramClient('19', 12899094, '335e73259ab7fe48c3d12e4139f6dfa0') 
-  client.connect()
-  print('connected')
+  result='none'
+  try:
+    c=TelegramClient('73',73636,'7373737737338')
+    c.connect()
+    result='connected'
+  except Exception as e:
+    result=str(e)
+  return result
 
-main()
