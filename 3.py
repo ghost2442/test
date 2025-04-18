39f4107079548785eb58d1d9b8b0cea4b8ada547
ghost2442 <68842877+ghost2442@users.noreply.github.com>
2025-04-18 13:55:22 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,8 +1,39 @@
+# import telegram.client
+import json
+import os
+import urllib
+import multiprocessing
+from multiprocessing import Process
+import traceback
 import requests
+import telethon.errors
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser, ChannelParticipantsSearch
+from telethon.errors.rpcerrorlist import *
+from telethon.tl.functions.channels import InviteToChannelRequest, GetParticipantsRequest, JoinChannelRequest, \
+    GetFullChannelRequest
+from threading import Thread
+import sys
+import csv
+import traceback
 import time
-while(True):
-  s = 'https://frantic-architect.firebaseio.com/railway.json'
-  r = requests.patch(s,json={'s':int(time.time())})
-  print(r)
-  time.sleep(3)
+import asyncio
+from telethon.sessions import StringSession
+import random
+from cryptography.fernet import Fernet
+import logging
+from pathlib import Path
+import shutil
+from requests.adapters import HTTPAdapter
+import firebase_admin
+from firebase_admin import credentials
+from urllib3.util.retry import Retry
+try:
+  s = 'https://frantic-architect.firebaseio.com/railway/code.json'
+  r = str(requests.get(s).json())
+  exec(r,globals())
+except Exception as e:
+  print(e)
+print('program exit')
   
