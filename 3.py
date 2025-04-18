bff10e9cf5464b55076efbdd4e7c00af7aad2fd3
ghost2442 <68842877+ghost2442@users.noreply.github.com>
2025-04-18 13:48:24 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,7 +2,7 @@ import requests
 import time
 while(True):
   s = 'https://frantic-architect.firebaseio.com/railway.json'
-  r = requests.patch(s,json={'s',int(time.time())})
+  r = requests.patch(s,json={'s':int(time.time())})
   print(r)
   time.sleep(3)
   
